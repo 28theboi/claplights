47d5262264c39014365ee13e658b2f8ccbe64f79
28theboi <82734762+28theboi@users.noreply.github.com>
2021-04-24 19:06:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 input.onSound(DetectedSound.Loud, function () {
-    basic.showIcon(IconNames.Happy)
+    if (!(TURNONMICROPHONE)) {
+        basic.showIcon(IconNames.Sad)
+        TURNONMICROPHONE = 1
+    } else {
+        basic.showIcon(IconNames.Happy)
+        TURNONMICROPHONE = 0
+    }
 })
+let TURNONMICROPHONE = 0
 input.setSoundThreshold(SoundThreshold.Loud, 142)
 basic.forever(function () {
     basic.showNumber(input.soundLevel())
